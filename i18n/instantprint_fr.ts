5e306f777716acf06a6da9c87269ee570cef9dc2
Christophe55 <christophe.maginot@gmail.com>
2016-02-28 19:34:41 +0100

    Update instantprint_fr.ts

--- i18n/instantprint_fr.ts
+++ i18n/instantprint_fr.ts
@@ -1,26 +1,26 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="de" sourcelanguage="">
+<!DOCTYPE TS><TS version="2.0" language="fr" sourcelanguage="">
 <context>
     <name>InstantPrintDialog</name>
     <message>
         <location filename="ui/ui_printdialog.py" line="69"/>
         <source>Instant Print</source>
-        <translation>Sofortdruck</translation>
+        <translation>Impression rapide</translation>
     </message>
     <message>
         <location filename="ui/ui_printdialog.py" line="70"/>
         <source>Composer:</source>
-        <translation>Zusammenstellung:</translation>
+        <translation>Composeur d'impression:</translation>
     </message>
     <message>
         <location filename="ui/ui_printdialog.py" line="71"/>
         <source>File format:</source>
-        <translation>Dateiformat:</translation>
+        <translation>Format du fichier:</translation>
     </message>
     <message>
         <location filename="ui/ui_printdialog.py" line="72"/>
         <source>Scale:</source>
-        <translation>Skala:</translation>
+        <translation>Echelle:</translation>
     </message>
 </context>
 <context>
@@ -28,7 +28,7 @@
     <message>
         <location filename="InstantPrintPlugin.py" line="42"/>
         <source>Instant Print</source>
-        <translation>Sofortdruck</translation>
+        <translation>Impression rapide</translation>
     </message>
 </context>
 <context>
@@ -36,57 +36,57 @@
     <message>
         <location filename="InstantPrintTool.py" line="190"/>
         <source>Print Composition</source>
-        <translation>Zusammenstellung drucken</translation>
+        <translation>Imprimer la composition</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="212"/>
         <source>Print Failed</source>
-        <translation>Druck gescheitert</translation>
+        <translation>Echec d'impression</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="212"/>
         <source>Failed to print the composition.</source>
-        <translation>Die Zusammenstellung konnte nicht gedruckt werden.</translation>
+        <translation>L'impression a échouée.</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="34"/>
         <source>Export</source>
-        <translation>Exportieren</translation>
+        <translation>Exporter</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="35"/>
         <source>Help</source>
-        <translation>Hilfe</translation>
+        <translation>Aide</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="36"/>
         <source>PDF Document (*.pdf);;</source>
-        <translation>PDF Dokument (*.pdf);;</translation>
+        <translation>Document PDF (*.pdf);;</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="37"/>
         <source>JPG Image (*.jpg);;</source>
-        <translation>JPG Bild (*.jpg);;</translation>
+        <translation>Image JPG (*.jpg);;</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="38"/>
         <source>BMP Image (*.bmp);;</source>
-        <translation>BMP Bild (*.bmp);;</translation>
+        <translation>Image BMP (*.bmp);;</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="39"/>
         <source>PNG Image (*.png);;</source>
-        <translation>PNG Bild (*.png);;</translation>
+        <translation>Image PNG (*.png);;</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="87"/>
         <source>Invalid composer</source>
-        <translation>Ungültige Druckzusammenstellung</translation>
+        <translation>Composer d'impression invalide</translation>
     </message>
     <message>
         <location filename="InstantPrintTool.py" line="87"/>
         <source>The composer must have exactly one map item.</source>
-        <translation>Die Zusammenstellung muss genau ein Kartenelement haben.</translation>
+        <translation>Le composeur d'impression ne doit avoir qu'une seule carte.</translation>
     </message>
 </context>
 </TS>
